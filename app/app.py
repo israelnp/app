87755c46e69d9e75a0d2fac1bc0b0811fcabb2e8
onx solutions <solutions.onx.jira@gmail.com>
2022-03-07 02:15:04 -0300

    Adicionando export do prometheus no app

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-
+metrics = PrometheusMetrics(app)
 @app.route("/")
 def hello_world():
     get_command_linux = os.popen('hostname')
